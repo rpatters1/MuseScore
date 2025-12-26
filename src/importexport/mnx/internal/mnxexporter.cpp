0048ec813e490afdfcfe9fe513ba06a5dce0b0e6
Robert Patterson <robert@robertgpatterson.com>
2025-12-25 18:06:48 -0600

    add importer and exporter

---
+++ src/importexport/mnx/internal/mnxexporter.cpp
@@ -0,0 +1,40 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-Studio-CLA-applies
+ *
+ * MuseScore Studio
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2021 MuseScore Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "mnxexporter.h"
+
+#include "engraving/dom/score.h"
+
+#include "mnxdom.h"
+
+using namespace mu::engraving;
+using namespace ::mnx;
+
+namespace mu::iex::mnx {
+
+void MnxExporter::exportMnx()
+{
+    // Header
+    MnxMetaData::Support support = m_mnxDocument.mnx().create_support();
+    support.set_useBeams(true);
+}
+
+} // namespace mu::iex::mnx
